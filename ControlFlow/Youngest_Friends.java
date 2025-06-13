5c4f3988ff259af5b38c6e1de276b2679cbc2ceb
krishna gupta <krishna.gupta_cs22@gla.ac.in>
2025-06-13 10:15:28 +0530

    [krishna] Add.newFile

---
+++ ControlFlow/Youngest_Friends.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Youngest_Friends{
+ public Static void main(String[] args){
+ Scanner input= new Scanner(System.in);
+ int amarAge = sc.nextInt();
+ int anthonyAge= sc.nextInt();
+ int akbarAge= sc.nextInt();
+ double amarHeight= sc.nextDouble();
+ double akbarHeight= sc.nextDouble();
+ double anthonyHeight= sc.nextDouble();
+ if(amarAge>anthonyAge && akbarAge>anthonyAge){
+  System.out.print("Amar is youngest one");}
+  else if(amarAge>akbarAge && anthonyAge>akbarAge){
+  System.out.print("Akbar is youngest one");}
+else{
+ System.out.print("Anthony is youngest one");}
+   if(amarHeight>anthonyHeight && amarHeight>anthonyHeight){
+  System.out.print("Amar is Longest one");}
+ else if(akbarHeight>anthonyHeight && akbarHeight>amarHeight){
+  System.out.print("Akbar is Longest one");}
+else{
+ Suytem.out.print("anthony is longest one")}
+  }}
+
+ 
+
+
+
+ 
