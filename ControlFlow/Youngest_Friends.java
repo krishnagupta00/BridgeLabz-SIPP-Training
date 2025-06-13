9e618052df5b50e4606f90280fec95fc5b23c48e
krishna gupta <krishna.gupta_cs22@gla.ac.in>
2025-06-13 10:50:02 +0530

    [krishna] Add.newFile

--- ControlFlow/Youngest_Friends.java
+++ ControlFlow/Youngest_Friends.java
@@ -1,29 +1,37 @@
 import java.util.*;
-public class Youngest_Friends{
- public Static void main(String[] args){
- Scanner input= new Scanner(System.in);
- int amarAge = sc.nextInt();
- int anthonyAge= sc.nextInt();
- int akbarAge= sc.nextInt();
- double amarHeight= sc.nextDouble();
- double akbarHeight= sc.nextDouble();
- double anthonyHeight= sc.nextDouble();
- if(amarAge>anthonyAge && akbarAge>anthonyAge){
-  System.out.print("Amar is youngest one");}
-  else if(amarAge>akbarAge && anthonyAge>akbarAge){
-  System.out.print("Akbar is youngest one");}
-else{
- System.out.print("Anthony is youngest one");}
-   if(amarHeight>anthonyHeight && amarHeight>anthonyHeight){
-  System.out.print("Amar is Longest one");}
- else if(akbarHeight>anthonyHeight && akbarHeight>amarHeight){
-  System.out.print("Akbar is Longest one");}
-else{
- Suytem.out.print("anthony is longest one")}
-  }}
 
- 
+public class Youngest_Friends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        // Input ages
+        int amarAge = sc.nextInt();
+        int anthonyAge = sc.nextInt();
+        int akbarAge = sc.nextInt();
 
+        // Input heights
+        double amarHeight = sc.nextDouble();
+        double anthonyHeight = sc.nextDouble();
+        double akbarHeight = sc.nextDouble();
 
- 
+        // Finding the youngest
+        if (amarAge < anthonyAge && amarAge < akbarAge) {
+            System.out.println("Amar is the youngest one");
+        } else if (akbarAge < amarAge && akbarAge < anthonyAge) {
+            System.out.println("Akbar is the youngest one");
+        } else {
+            System.out.println("Anthony is the youngest one");
+        }
+
+        // Finding the tallest
+        if (amarHeight > anthonyHeight && amarHeight > akbarHeight) {
+            System.out.println("Amar is the tallest one");
+        } else if (akbarHeight > anthonyHeight && akbarHeight > amarHeight) {
+            System.out.println("Akbar is the tallest one");
+        } else {
+            System.out.println("Anthony is the tallest one");
+        }
+
+        sc.close(); // Always good to close Scanner
+    }
+}
