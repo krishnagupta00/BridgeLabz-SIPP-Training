23b61193e50b6d75350c9aa22e4e072b17a0df42
krishna gupta <krishna.gupta_cs22@gla.ac.in>
2025-07-14 10:37:19 +0530

    Add linkedList only

--- Day1/Level1/Problem3.java
+++
@@ -1,7 +0,0 @@
-
- public class Problem3{
- public static void main(String[] args){
- double kiloMeters = 10.8;
- double miles = 1.6 * kiloMeters;
- System.out.print("The distance " + kiloMeters+" km in miles is" + miles);
-}}
