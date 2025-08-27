e1344156e49ecb4c2e80908297f9fcb3ae98d3d5
krishna gupta <krishna.gupta_cs22@gla.ac.in>
2025-08-27 10:23:54 +0530

    add week 2 day 1 assignment

--- linkedList/RouteTracker.java
+++ linkedList/RouteTracker.java
@@ -2,4 +2,13 @@ import java.util.*;
 Public class RouteTracker{
 public static void main(String[] args){
  LinkedList<Integer> input=new Linkedlist<>();
+
+    public Checkpoint(String checkpointId, String locationName, double distanceFromLast,
+                      int expectedDuration, int actualDuration) {
+        this.checkpointId = checkpointId;
+        this.locationName = locationName;
+        this.distanceFromLast = distanceFromLast;
+        this.expectedDuration = expectedDuration;
+        this.actualDuration = actualDuration;
+    }
   }}
